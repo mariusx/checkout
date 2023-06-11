b55af4d8898fde2d1e85d950cdf3bd30435e8743
Marius Jakobsen <marius.jakobsen@gmail.com>
2023-06-11 22:33:06 +0200

    refactor: Enable integration test after implementation of discount calculation

--- app/src/test/java/biz/marius/ecom/checkout/controller/CheckoutControllerIntegrationTest.java
+++ app/src/test/java/biz/marius/ecom/checkout/controller/CheckoutControllerIntegrationTest.java
@@ -1,6 +1,5 @@
 package biz.marius.ecom.checkout.controller;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,12 +11,11 @@ import org.springframework.test.web.reactive.server.WebTestClient;
 class CheckoutControllerIntegrationTest {
   private static final String CHECKOUT_URI = "/checkout";
   private static final int UNSUPPORTED_MEDIA_TYPE = 415;
-  private static final String ORDER_LINES_WATCHES = "[\"001\", \"002\", \"003\"]";
-  private static final String EXPECTED_CHECKOUT_RESPONSE = "{\"price\":230}";
+  private static final String ORDER_LINES_WATCHES = "[\"001\", \"001\", \"001\", \"002\", \"003\"]";
+  private static final String EXPECTED_CHECKOUT_RESPONSE = "{\"price\":330}";
   @Autowired
   private WebTestClient webClient;
 
-  @Disabled("TODO: Enable when discount implemented") //TODO: Enable when discount implemented
   @Test
   void should_return_200_for_checkout() {
     webClient.post()
