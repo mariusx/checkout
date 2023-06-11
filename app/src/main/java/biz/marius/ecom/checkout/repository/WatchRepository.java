d9d1ebd1586690369696bad6aaff81fc2b7a617f
Marius Jakobsen <marius.jakobsen@gmail.com>
2023-06-11 22:29:25 +0200

    feat: Adds static content to WatchRepository

--- app/src/main/java/biz/marius/ecom/checkout/repository/WatchRepository.java
+++ app/src/main/java/biz/marius/ecom/checkout/repository/WatchRepository.java
@@ -5,10 +5,30 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
 
 @Repository
 public class WatchRepository {
-  public Map<String, List<Watch>> findByIdIn(List<String> distinctWatches) {
-    return Map.of();
+  private final Map<String, Watch> watches = new ConcurrentHashMap<>();
+
+  public WatchRepository() {
+    watches.put("001", new Watch("001", 100));
+    watches.put("002", new Watch("002", 80));
+    watches.put("003", new Watch("003", 50));
+    watches.put("004", new Watch("004", 30));
+
   }
+
+  /**
+   * Find by ids in list and return Map grouped with List of Watches
+   * @param ids Ids to find
+   * @return Watches grouped by id
+   */
+  public Map<String, List<Watch>> findByIdIn(List<String> ids) {
+    return ids.stream()
+        .filter(watches::containsKey)
+        .map(watches::get).collect(Collectors.groupingBy(Watch::getId));
+  }
+
 }
